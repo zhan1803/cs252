2d5b7e24016c7d6b3c59462939136e596d1d8fa4
zhan1803 <zhan1803@purdue.edu>
2016-12-01 14:39:11 -0500

    side nave bar, search box and create button

--- public/homeController.js
+++ public/homeController.js
@@ -1,6 +1,27 @@
 var app = angular.module('rideShare');
-app.controller('MyController', function($scope, $mdSidenav) {
+app.controller('SideNavController', function($scope, $mdSidenav) {
   $scope.openLeftMenu = function() {
     $mdSidenav('left').toggle();
   };
+  $scope.openOfferMenu = function() {
+    $mdSidenav('offer').toggle();
+  };
+  $scope.openLookUpMenu = function() {
+    $mdSidenav('lookup').toggle();
+  };
+  $scope.openAboutUsMenu = function() {
+    $mdSidenav('aboutus').toggle();
+  };
+  $scope.openContactMenu = function() {
+    $mdSidenav('contact').toggle();
+  };
 });
+
+
+
+/*
+ function openNav() {
+ document.getElementById("mySidenav").style.width = "250px";
+ }
+
+ */
