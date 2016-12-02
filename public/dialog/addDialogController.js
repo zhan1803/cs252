1b006da75567c7da3f7efbf79ef04bd6ee3dbe55
Dennis Chia <Dennis Chia>
2016-12-02 00:18:15 -0500

    added new dialog for offer, changed server to accomodate get request, changed home page view to use ng-repeat

--- public/dialog/addDialogController.js
+++ public/dialog/addDialogController.js
@@ -1,13 +1,22 @@
 (function(){
 	angular.module('rideShare')
-	.controller('addDialogController', function($http,$mdDialog) {
+	.controller('addDialogController', function($http,$mdDialog,$filter,type) {
 		var vm = this;
+		vm.newPost= {
+			creationTime: '',
+			type: type,
+			trip: '',
+		};
 		vm.post = function() {
+			vm.date = new Date();
+			vm.date = $filter('date')(vm.date, "dd/MM/yyyy");
+			vm.newPost.creationTime = vm.date;
+			vm.newPost.trip = vm.trip;
 			console.log(vm.newPost);
 			$http.put('/post', vm.newPost);
 			$mdDialog.hide();
 		};
 
-		vm.type = 'OneWay';
+		vm.trip = 'OneWay';
 	});
 })()
